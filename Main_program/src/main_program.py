e99a0bdde697a0508bc17a8dd66943cdf97bdc3d
D.M <d.a.w.markus@student.tue.nl>
2017-09-07 14:49:06 +0200

    Test commit push and pull

--- Main_program/src/main_program.py
+++ Main_program/src/main_program.py
@@ -2,5 +2,5 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 
-if __name__ == "__main__":
-    print "Hello World"
+print("Hello World")
+print("Hessel is een home")
