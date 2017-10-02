89adefdcf6b5b3a130cf41f911aa52e8f1f2079f
Gijs Kok <31617118+SlebbaDestroyer69@users.noreply.github.com>
2017-10-02 12:47:32 +0200

    no message

--- ESP32_Screen/ESP32_Screen.ino
+++ ESP32_Screen/ESP32_Screen.ino
@@ -4,8 +4,12 @@
 
 SSD1306  display(0x3c, 21, 22);
 
-int score = 1;
+int k = 1;
+int score = 0;
 int lives = 3;
+
+String role;;
+bool quarantine;
 
 int LED1 = 2;
 int LED2 = 4;
@@ -13,36 +17,25 @@ int LED3 = 5;
 
 void setup() 
 {
-  Serial.begin(115200);
-  display.init();
-  display.flipScreenVertically();
-  display.setFont(ArialMT_Plain_24);
+    Serial.begin(115200);
+    display.init();
+    display.flipScreenVertically();
+    display.setFont(ArialMT_Plain_24);
   
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
-  pinMode(LED3, OUTPUT);
+    pinMode(LED1, OUTPUT);
+    pinMode(LED2, OUTPUT);
+    pinMode(LED3, OUTPUT);
+
+    ShowStart();
+    Role();
 }
 
-void ShowScore()  
+void ShowStart() 
 {
     display.clear();
-    display.setTextAlignment(TEXT_ALIGN_CENTER);
-    display.drawString(64, 15, String(score));
+    display.drawXbm(0, 0, 128, 64, Startup);
     display.display();
-}
-
-void RolePacman() 
-{
-    display.clear();
-    display.drawXbm(0, 0, 128, 64, Pacman);
-    display.display();
-}
-
-void RoleGhost() 
-{
-    display.clear();
-    display.drawXbm(0, 0, 128, 64, Ghost);
-    display.display();
+    delay(4000);
 }
 
 void ShowWinner() 
@@ -52,34 +45,75 @@ void ShowWinner()
     display.display();
 }
 
-void DisplayLives()
+void Role() 
 {
+    display.clear();
+    if (role == "pacman")
+    {
+      display.drawXbm(0, 0, 128, 64, Pacman);
+    }
+    else if(role == "ghost")
+    {
+      display.drawXbm(0, 0, 128, 64, Ghost);
+    }
+    display.display();
+    delay(3000);
+}
+
+void UI()  
+{
+    display.clear();
+    if (role == "pacman")
+    {
+      display.drawXbm(0, 0, 128, 64, Pacman_Small);
+    }
+    else if(role == "ghost")
+    {
+      display.drawXbm(0, 0, 128, 64, Ghost_Small);
+    }
+    display.setTextAlignment(TEXT_ALIGN_CENTER);
+    display.drawString(64, 25, String(score));
+    display.display();
     switch(lives)
     {
-        case 3:
-          digitalWrite (LED1, HIGH);
-          digitalWrite (LED2, HIGH); 
-          digitalWrite (LED3, HIGH);  
-        break;
-        case 2:
-          digitalWrite (LED1, HIGH);
-          digitalWrite (LED2, HIGH); 
-          digitalWrite (LED3, LOW);  
-        break;
-        case 1:
-          digitalWrite (LED1, HIGH);
-          digitalWrite (LED2, LOW); 
-          digitalWrite (LED3, LOW);  
-        break;
-        case 0:
-          digitalWrite (LED1, LOW);
-          digitalWrite (LED2, LOW); 
-          digitalWrite (LED3, LOW); 
-        break;
+      case 3:
+        digitalWrite (LED1, HIGH);
+        digitalWrite (LED2, HIGH); 
+        digitalWrite (LED3, HIGH);  
+      break;
+      case 2:
+         digitalWrite (LED1, HIGH);
+         digitalWrite (LED2, HIGH); 
+         digitalWrite (LED3, LOW);
+      break;
+      case 1:
+         digitalWrite (LED1, HIGH);
+         digitalWrite (LED2, LOW); 
+         digitalWrite (LED3, LOW);  
+      break;
+      case 0:
+        digitalWrite (LED1, LOW);
+        digitalWrite (LED2, LOW); 
+        digitalWrite (LED3, LOW); 
+      break;
     }
 }
 
 void loop() 
 {
-  ShowWinner();
+  while(k != 0)
+  {
+    UI();
+    score++;
+    if(score == 5000){
+      lives = 2;
+    }
+    if(score == 10000){
+      lives = 1;
+    }
+    if(score == 18000){
+      ShowWinner();
+      k=0;
+    }
+  }
 }
