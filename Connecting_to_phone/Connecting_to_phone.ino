50b32cd9edef6fddd56b737fc203016c3ffdafc0
D.M <d.a.w.markus@student.tue.nl>
2017-10-04 13:03:34 +0200

    Writing of the server library for arduino

--- Connecting_to_phone/Connecting_to_phone.ino
+++ Connecting_to_phone/Connecting_to_phone.ino
@@ -1,16 +1,21 @@
+#include <ArduinoJson.h>
+
 
 
 #include <WiFi.h>
 #include "esp_wpa2.h"
 #include <HTTPClient.h>
 #include <ESP32WebServer.h>
+#include "PacmanServer.h"
+#include <ArduinoJson.h>
+/*
 
-ESP32WebServer server(50001);
+ESP32WebServer server1(50001);
 
 
 const char* ssid = "tue-wpa2"; // your ssid
 #define EAP_ID "d.a.w.markus@student.tue.nl"
-#define EAP_USERNAME "s169013"
+#define EAP_USERNAME ""
 #define EAP_PASSWORD ""
 
 char* host = NULL;
@@ -18,87 +23,88 @@ String message = "";
 int httpCode = 0;
 HTTPClient http;
 int i=0;
+//PacmanServer tu("lol");
 
 void event_location(){
   i++;
   Serial.print("Got request: ");
   Serial.println(i);
   Serial.println("Location request");
-  Serial.println(server.args());
-  Serial.println(server.arg(0));
-  server.send(200,"application/json; charset=utf-8", "hello from esp8266!");
+  Serial.println(server1.args());
+  Serial.println(server1.arg(0));
+  server1.send(200,"application/json; charset=utf-8", "hello from esp8266!");
 }
 void event_food(){
   i++;
   Serial.print("Got request: ");
   Serial.println(i);
   Serial.println("Food request");
-  Serial.println(server.args());
-  Serial.println(server.arg(0));
-  server.send(200,"application/json; charset=utf-8", "hello from esp8266!");
+  Serial.println(server1.args());
+  Serial.println(server1.arg(0));
+  server1.send(200,"application/json; charset=utf-8", "hello from esp8266!");
 }
 void event_cherry(){
   i++;
   Serial.print("Got request: ");
   Serial.println(i);
   Serial.println("Cherry request");
-  Serial.println(server.args());
-  Serial.println(server.arg(0));
-  server.send(200,"application/json; charset=utf-8", "hello from esp8266!");
+  Serial.println(server1.args());
+  Serial.println(server1.arg(0));
+  server1.send(200,"application/json; charset=utf-8", "hello from esp8266!");
 }
 void event_energizer(){
   i++;
   Serial.print("Got request: ");
   Serial.println(i);
   Serial.println("Energizer request");
-  Serial.println(server.args());
-  Serial.println(server.arg(0));
-  server.send(200,"application/json; charset=utf-8", "hello from esp8266!");
+  Serial.println(server1.args());
+  Serial.println(server1.arg(0));
+  server1.send(200,"application/json; charset=utf-8", "hello from esp8266!");
 }
 void event_cherry_spawned(){
   i++;
   Serial.print("Got request: ");
   Serial.println(i);
   Serial.println("Cherry Spawned request");
-  Serial.println(server.args());
-  Serial.println(server.arg(0));
-  server.send(200,"application/json; charset=utf-8", "hello from esp8266!");
+  Serial.println(server1.args());
+  Serial.println(server1.arg(0));
+  server1.send(200,"application/json; charset=utf-8", "hello from esp8266!");
 }
 void event_collision(){
   i++;
   Serial.print("Got request: ");
   Serial.println(i);
   Serial.println("Collision request");
-  Serial.println(server.args());
-  Serial.println(server.arg(0));
-  server.send(200,"application/json; charset=utf-8", "hello from esp8266!");
+  Serial.println(server1.args());
+  Serial.println(server1.arg(0));
+  server1.send(200,"application/json; charset=utf-8", "hello from esp8266!");
 }
 void event_quarantine(){
   i++;
   Serial.print("Got request: ");
   Serial.println(i);
   Serial.println("Quarantine request");
-  Serial.println(server.args());
-  Serial.println(server.arg(0));
-  server.send(200,"application/json; charset=utf-8", "hello from esp8266!");
+  Serial.println(server1.args());
+  Serial.println(server1.arg(0));
+  server1.send(200,"application/json; charset=utf-8", "hello from esp8266!");
 }
 void event_game_over(){
   i++;
   Serial.print("Got request: ");
   Serial.println(i);
   Serial.println("Game over request");
-  Serial.println(server.args());
-  Serial.println(server.arg(0));
-  server.send(200,"application/json; charset=utf-8", "hello from esp8266!");
+  Serial.println(server1.args());
+  Serial.println(server1.arg(0));
+  server1.send(200,"application/json; charset=utf-8", "hello from esp8266!");
 }
 void event_game_won(){
   i++;
   Serial.print("Got request: ");
   Serial.println(i);
   Serial.println("Game won request");
-  Serial.println(server.args());
-  Serial.println(server.arg(0));
-  server.send(200,"application/json; charset=utf-8", "hello from esp8266!");
+  Serial.println(server1.args());
+  Serial.println(server1.arg(0));
+  server1.send(200,"application/json; charset=utf-8", "hello from esp8266!");
 }
 void setup() {
   // put your setup code here, to run once:
@@ -129,22 +135,22 @@ void setup() {
   Serial.println(WiFi.localIP());
   //wifi is setup
 
-  //setup http server
-  server.begin();
+  //setup http server1
+  server1.begin();
   
   //setting up all listing ports
-  server.on("/event/location", event_location);
-  server.on("/event/food", event_food);
-  server.on("/event/cherry", event_cherry);
-  server.on("/event/energizer", event_energizer);
-  server.on("/event/cherry_spawned", event_cherry_spawned);
-  server.on("/event/collision", event_collision);
-  server.on("/event/quarantine", event_quarantine);
-  server.on("/event/game_over", event_game_over);
-  server.on("/event/game_won", event_game_won);
+  server1.on("/event/location", event_location);
+  server1.on("/event/food", event_food);
+  server1.on("/event/cherry", event_cherry);
+  server1.on("/event/energizer", event_energizer);
+  server1.on("/event/cherry_spawned", event_cherry_spawned);
+  server1.on("/event/collision", event_collision);
+  server1.on("/event/quarantine", event_quarantine);
+  server1.on("/event/game_over", event_game_over);
+  server1.on("/event/game_won", event_game_won);
 
 
-  //register to server
+  //register to server1
   host = "http://pacman.autonomic-networks.ele.tue.nl/register";
   Serial.print("connecting to ");
   Serial.println(host);  
@@ -164,16 +170,46 @@ void setup() {
     }
   
   http.end();
-  
+
+
 }
 
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+  host = "http://multipacman.nl:5000/new_direction";
+  http.begin(host);
+  http.useHTTP10(false);
+  message = "{\"direction\":[{\"direction\":\"Hessel is echt tering lelijk\",\"test_data\":\"test\"}]}";
+  httpCode = 0;
+  http.addHeader("Content-Type", "application/json");
+  httpCode = http.POST(message);
+  if(httpCode > 0){
+  Serial.printf("[HTTP] POST... code: %d\n", httpCode);
+  if(httpCode == HTTP_CODE_OK) {
+            String payload = http.getString();
+            Serial.println(payload);
+        }
+    } else {
+        Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
+        Serial.print("code: ");
+        Serial.println(httpCode);
+    }
+  http.end();  
 
-  server.handleClient();
+  //server1.handleClient();
   delay(50);
   
   
+}*/
+
+void setup()
+{
 }
+
+void loop ()
+{
+  
+}
+
+
